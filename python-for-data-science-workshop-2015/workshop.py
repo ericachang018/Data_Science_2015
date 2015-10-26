3f834990b1784d9f4f0157ddac6bbf2c7aa31568
Erica Chang <Erica@Ericas-MacBook-Pro.local>
2015-10-26 09:03:57 -0400

    Workshop to prep for the GA Data Science Workshop

---
+++ python-for-data-science-workshop-2015/workshop.py
@@ -0,0 +1,145 @@
+# if / else 
+# x = 5 
+# if x > 4: 
+# 	print 'hello'
+# else: 
+# 	print 'world'
+
+
+# if x in [1,2,3,4,7]: 
+# 	print "x is in this list"
+# else: 
+# 	print "x is not in this list"  
+
+
+#Given x = "John Jameson",
+# - Construct an if else statement according to this logic: if 
+#"John" is in x, print "John is in x", otherwise print "John is 
+# not in x"
+# - Construct an if/elif/else statement according to this logic: 
+#if "roger" is in x, print "Hi Roger!", elif the length of x is 
+#greater than 20, print "thats a long string", else print "Oh well!"
+
+
+# x = 'John Jameson'
+
+# if 'John' in x:
+# 	print "John is in x"
+# else: 
+# 	print "John is not in x"
+
+
+# if 'roger' in x: 
+# 	print "Hi Roger!"
+# elif len(x) > 20:
+# 	print "thats a long string"
+# else:
+# 	print "oh well"
+
+
+######### Loops! They are awesome ########
+
+ # homework - do 10 of these problems https://projecteuler.net/archives 
+
+# numbers = [1,2 3, 7, 9, 10 ]
+
+# odds = []
+
+# for dog in numbers: 
+# 	if % 2 == 1: 
+# 		odds.append(dog)
+
+# print odds 
+
+#Construct a list of numbers between 0 and 1000 that are divisible by 33
+
+# numbers = range(1000)
+# div_by_33 = []
+
+# for i in numbers:
+# 	if i % 33 == 0:
+# 		div_by_33.append(i)
+# print div_by_33
+
+#loop 2 exercise comptute the sum of numbers btw 0 - 10000 that 
+# are even of divisable by 5
+
+# numbers = range(10000)
+# divisable = []
+
+# for i in numbers: 
+# 	if i % 2 == 0 or i % 5 == 0:
+# 		divisable.append(i)
+
+
+# total = sum(divisable)	
+# print total
+
+# #or you can do 
+# sum_of_loop = 0 
+# for x in range(1000):
+# 	if i % 2 == 0 or i % 5 == 0:
+# 		sum_of_loop += i 
+
+# # list comp
+
+# resutls = [i for i in range(10000) if i % 2 == 0 or i % 5 == 0 ]
+
+# max(i for i in range (10000) if i % 2 == 0 or i % 5 == 0  )
+
+
+#### Functions! #####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
